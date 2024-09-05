6d16bb0a9c37c338ef95d6d8b84e1e79150136f1
Thiemann <Thiemann.Tobias@bauer-suedlohn.com>
2024-09-05 10:22:10 +0200

    move test script

--- test_scripts/plant1_totaloutput.py
+++ test_scripts/plant1_totaloutput.py
@@ -6,17 +6,22 @@ from SmaDataManagerMClient.client import SMAApiClient
 async def testfunction():
     cred = json.load(open("credentials.json"))
 
+    print(cred["email"])
+    print(cred["password"])
+
     api = SMAApiClient(
         host="192.168.1.169",
         username=cred["email"],
         password=cred["password"]
     )
 
-    try:
-        await api.login()
-    except:
-        exit()
+    await api.login()
 
-    print(json.dumps( await api.get_all_live_measurements(), indent=4 ))
+    ids = []
+
+    for component in await api.get_all_components():
+        ids.append(component.component_id)
+
+    print(await api.get_all_live_measurements(["Plant:1"]))
 
 asyncio.run(testfunction())
