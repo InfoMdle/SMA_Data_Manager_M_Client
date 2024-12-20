6bd4846eeb1ffc63e12b57adc58024e30a0a5574
Amaury DROUIN <a.drouin@manufacturedelephemere.fr>
2024-12-20 12:31:18 +0100

    correction du fichier init

--- custom_components/sma_data_manager_m/__init__.py
+++ custom_components/sma_data_manager_m/__init__.py
@@ -1,1 +1,20 @@
 """SMA Api client."""
+"""Initialize the SMA Data Manager M integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the SMA Data Manager M component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up SMA Data Manager M from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
